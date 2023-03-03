293bc7ed1a069f340094e2cd2088df03cf15f615
nirmal <nirmal.seneviratne@gmail.com>
2023-03-03 21:19:10 +0530

    Add translation API support.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'To-Do-List-App';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    const params = new HttpParams()
+      .append('key', 'AIzaSyCv5PW04RpJ_wDP6gu1tzOMZRHFsZeUMN0')
+      .append('q', 'what is your age')
+      .append('source', 'en')
+      .append('target', 'de');
+    
+    this.http.post('https://translation.googleapis.com/language/translate/v2', null, { params: params } )
+    .subscribe(result => console.log(result));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
